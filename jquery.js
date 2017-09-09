b3b206657096e209a468e66454f33a58b803bda5
Kevin Grastorf <kevin.grastorf@gmail.com>
2017-09-09 12:32:54 -0600

    doc.ready

--- jquery.js
+++ jquery.js
@@ -1,24 +1,28 @@
-jQuery('#bckd-img1').mouseenter(function() {
-    jQuery('#hero').css("background-image", "url(./images/rose.jpg)");
+
+$(document).ready(function() {
+$('#bckd-img1').mouseenter(function() {
+    $('#hero').css("background-image", "url(./images/rose.jpg)");
 });
 
 
-jQuery('#bckd-img2').mouseenter(function() {
-    jQuery('#hero').css("background-image", "url(./images/KariFocus.jpg)");
+$('#bckd-img2').mouseenter(function() {
+    $('#hero').css("background-image", "url(./images/KariFocus.jpg)");
 });
 
 
-jQuery('#bckd-img3').mouseenter(function() {
-    jQuery('#hero').css("background-image", "url(./images/Feetblur2FocusInt.jpg");
+$('#bckd-img3').mouseenter(function() {
+    $('#hero').css("background-image", "url(./images/Feetblur2FocusInt.jpg");
 });
 
 
-jQuery('#bckd-img4').mouseenter(function() {
-    jQuery('#hero').css("background-image", "url(./images/BeetJuiceSpring.jpg)");
+$('#bckd-img4').mouseenter(function() {
+    $('#hero').css("background-image", "url(./images/BeetJuiceSpring.jpg)");
 });
 
 
 
-jQuery('#bckd-img5').mouseenter(function() {
-    jQuery('#hero').css("background-image", "url(./images/Carrots.jpg)");
+$('#bckd-img5').mouseenter(function() {
+    $('#hero').css("background-image", "url(./images/Carrots.jpg)");
+});
+
 });
